8317bbf4cf9b16db20edae7bf88daa376afabfef
Wakadaisho <paulwahome9@gmail.com>
2023-12-14 23:13:59 +0300

    Add default and not null

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-    country enum('US', 'CO', 'TN'),
+    country enum('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 	PRIMARY KEY (id)
 );
