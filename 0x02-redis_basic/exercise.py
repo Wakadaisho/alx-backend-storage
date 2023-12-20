d996b7b59d4e482d3e88f83e2e794624718fc797
Wakadaisho <paulwahome9@gmail.com>
2023-12-20 21:23:04 +0300

    Add basic redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Redis Cache module
+"""
+import uuid
+from typing import Union, Callable
+from functools import wraps
+
+import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts how many times
+    methods of the Cache class are called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """A wrapper function"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and
+    outputs for a particular function.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """A wrapper function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
+
+class Cache:
+    """Class that manages a redis cache instance"""
+    def __init__(self):
+        self._redis = redis.Redis()
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int,
+                                                          float]:
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, int)
+
+
+def replay(method: Callable):
+    _redis = redis.Redis()
+    key = method.__qualname__
+    print("{} was called {} times".format(key,  int(_redis.get(key))))
+    inputs = _redis.lrange(key + ":inputs", 0, -1)
+    outputs = _redis.lrange(key + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(key, i.decode("utf-8"),
+                                     o.decode("utf-8")))
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Main file """
+
+from functools import wraps
+from typing import Callable
+
+import redis
+import requests
+
+
+def count_calls(method: Callable) -> Callable:
+    """Count number of times a url is called"""
+
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """A wrapper function"""
+        _redis = redis.Redis()
+        _redis.incr("count:{}".format(args))
+        _redis.expire("count:{}".format(args), 10)
+        return method(*args, **kwargs)
+
+    return wrapper
+
+
+@count_calls
+def get_page(url: str) -> str:
+    requests.get(url)
